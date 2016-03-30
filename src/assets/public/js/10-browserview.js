e0f9809e812d0ab0cf2b01712a9fd5f68926460f
PolyFloyd <floyd@polyfloyd.net>
2016-03-30 18:50:37 +0200

    Preserve the browser state when navigating

--- src/assets/public/js/10-browserview.js
+++ src/assets/public/js/10-browserview.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var BrowserView = Backbone.View.extend({
-	setState: function(state) { },
+	setState: function(state) {
+		this.trigger('change-state', state || '');
+	},
 
 	getState: function() { return ''; },
 });
