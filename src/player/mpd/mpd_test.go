f66ba7d6ebb8613d4ba649e71ede382c5295d30e
polyfloyd <floyd@polyfloyd.net>
2017-09-18 15:39:48 +0200

    player/mpd: Add more tests for player manipulation

--- src/player/mpd/mpd_test.go
+++ src/player/mpd/mpd_test.go
@@ -10,14 +10,73 @@ func connectForTesting() (*Player, error) {
 	return Connect("tcp", "127.0.0.1:6600", nil)
 }
 
+func TestTrackIndex(t *testing.T) {
+	pl, err := connectForTesting()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if err := player.TestTrackIndex(pl); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestTrackIndexEvent(t *testing.T) {
+	pl, err := connectForTesting()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if err := player.TestTrackIndexEvent(pl); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestPlaystate(t *testing.T) {
+	pl, err := connectForTesting()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if err := player.TestPlaystate(pl); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
 func TestPlaystateEvent(t *testing.T) {
 	pl, err := connectForTesting()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-
 	if err := player.TestPlaystateEvent(pl); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestVolume(t *testing.T) {
+	pl, err := connectForTesting()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if err := player.TestVolume(pl); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestVolumeEvent(t *testing.T) {
+	pl, err := connectForTesting()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if err := player.TestVolumeEvent(pl); err != nil {
 		t.Error(err)
 		return
 	}
--- src/player/player_testutils.go
+++ src/player/player_testutils.go
@@ -5,7 +5,120 @@ import (
 	"time"
 )
 
+func fillPlaylist(pl Player, numTracks int) error {
+	tracks, err := pl.Tracks()
+	if err != nil {
+		return err
+	}
+	length, err := pl.Playlist().Len()
+	if err != nil {
+		return err
+	}
+	rm := make([]int, length)
+	for i := range rm {
+		rm[i] = i
+	}
+	if err := pl.Playlist().Remove(rm...); err != nil {
+		return err
+	}
+	if len(tracks) < numTracks {
+		return fmt.Errorf("Not enough tracks in the library: %v < %v", len(tracks), numTracks)
+	}
+	return pl.Playlist().Insert(0, tracks[0:numTracks]...)
+}
+
+func TestTrackIndex(pl Player) error {
+	if err := fillPlaylist(pl, 2); err != nil {
+		return err
+	}
+	if err := pl.SetTrackIndex(0); err != nil {
+		return err
+	}
+	if index, err := pl.TrackIndex(); err != nil {
+		return err
+	} else if index != 0 {
+		return fmt.Errorf("Unexpected track index: %v != %v", 0, index)
+	}
+	if state, err := pl.State(); err != nil {
+		return err
+	} else if state != PlayStatePlaying {
+		return fmt.Errorf("Unexpected state: %v", state)
+	}
+
+	if err := pl.SetTrackIndex(1); err != nil {
+		return err
+	}
+	if index, err := pl.TrackIndex(); err != nil {
+		return err
+	} else if index != 1 {
+		return fmt.Errorf("Unexpected track index: %v != %v", 1, index)
+	}
+	return nil
+}
+
+func TestTrackIndexEvent(pl Player) error {
+	if err := fillPlaylist(pl, 2); err != nil {
+		return err
+	}
+
+	l := pl.Events().Listen()
+	defer pl.Events().Unlisten(l)
+
+	if err := pl.SetTrackIndex(1); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case msg := <-l:
+			if msg == "playlist" {
+				return nil
+			}
+		case <-time.After(time.Second):
+			return fmt.Errorf("Event was not emitted")
+		}
+	}
+}
+
+func TestPlaystate(pl Player) error {
+	if err := fillPlaylist(pl, 1); err != nil {
+		return err
+	}
+
+	if err := pl.SetState(PlayStatePlaying); err != nil {
+		return err
+	}
+	if state, err := pl.State(); err != nil {
+		return err
+	} else if state != PlayStatePlaying {
+		return fmt.Errorf("Unexpected state: %v", state)
+	}
+
+	if err := pl.SetState(PlayStatePaused); err != nil {
+		return err
+	}
+	if state, err := pl.State(); err != nil {
+		return err
+	} else if state != PlayStatePaused {
+		return fmt.Errorf("Unexpected state: %v", state)
+	}
+
+	if err := pl.SetState(PlayStateStopped); err != nil {
+		return err
+	}
+	if state, err := pl.State(); err != nil {
+		return err
+	} else if state != PlayStateStopped {
+		return fmt.Errorf("Unexpected state: %v", state)
+	}
+	return nil
+}
+
 func TestPlaystateEvent(pl Player) error {
+	if err := fillPlaylist(pl, 1); err != nil {
+		return err
+	}
+
 	l := pl.Events().Listen()
 	defer pl.Events().Unlisten(l)
 
@@ -20,6 +133,63 @@ func TestPlaystateEvent(pl Player) error {
 		select {
 		case msg := <-l:
 			if msg == "playstate" {
+				return nil
+			}
+		case <-time.After(time.Second):
+			return fmt.Errorf("Event was not emitted")
+		}
+	}
+}
+
+func TestVolume(pl Player) error {
+	const VOL_A = 0.2
+	const VOL_B = 0.4
+	if err := fillPlaylist(pl, 1); err != nil {
+		return err
+	}
+
+	if err := pl.SetState(PlayStatePlaying); err != nil {
+		return err
+	}
+	if err := pl.SetVolume(VOL_A); err != nil {
+		return err
+	}
+	if vol, err := pl.Volume(); err != nil {
+		return err
+	} else if vol != VOL_A {
+		return fmt.Errorf("Volume does not match expected value, %v != %v", VOL_A, vol)
+	}
+
+	if err := pl.SetState(PlayStateStopped); err != nil {
+		return err
+	}
+	if err := pl.SetVolume(VOL_B); err != nil {
+		return err
+	}
+	if vol, err := pl.Volume(); err != nil {
+		return err
+	} else if vol != VOL_B {
+		return fmt.Errorf("Volume does not match expected value, %v != %v", VOL_B, vol)
+	}
+	return nil
+}
+
+func TestVolumeEvent(pl Player) error {
+	if err := fillPlaylist(pl, 1); err != nil {
+		return err
+	}
+
+	l := pl.Events().Listen()
+	defer pl.Events().Unlisten(l)
+
+	if err := pl.SetVolume(0.2); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case msg := <-l:
+			if msg == "volume" {
 				return nil
 			}
 		case <-time.After(time.Second):
