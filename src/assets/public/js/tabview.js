852ebade8c0903adb27f3926ffd82e590092dfc1
polyfloyd <floyd@polyfloyd.net>
2019-04-14 15:16:05 +0200

    Rewrite TabView to ES6

--- src/assets/public/js/tabview.js
+++ src/assets/public/js/tabview.js
@@ -1,26 +1,25 @@
-'use strict';
-
-var TabView = Backbone.View.extend({
-	tagName:   'div',
-	className: 'tab-view',
+class TabView {
+	constructor() {
+		this.$el = $('<div class="tab-view">');
+	}
 
 	/**
-	 * Shows a tab (yes, really).
+	 * Shows a tab.
 	 *
 	 * If the tab is named and a tab with the same name already exists, it is
 	 * substituted. In adittion, all tabs to the right are removed.
 	 */
-	pushTab: function(content, options) {
+	pushTab(content, options) {
 		var self = this;
 		options || (options = {});
 
-		var $tab = $(this.tabTemplate(options)).append(content);
+		var $tab = $(TabView.tabTemplate(options)).append(content);
 		$tab.find('.do-pop-tab').on('click', (event) => {
 			event.stopPropagation();
 			self.popTab();
 		});
 
-		var $old = options.name && this.$('.tab[data-name="'+options.name.replace(/"/g, '\\"')+'"]');
+		var $old = options.name && this.$el.find('.tab[data-name="'+options.name.replace(/"/g, '\\"')+'"]');
 		if ($old.length) {
 			$old.after($tab);
 			$old.remove();
@@ -30,24 +29,24 @@ var TabView = Backbone.View.extend({
 
 		$tab.find('~').remove();
 		return $tab;
-	},
+	}
 
-	popTab: function() {
-		this.$('.tab:last-child').remove();
-	},
+	popTab() {
+		this.$el.find('.tab:last-child').remove();
+	}
 
-	clearTabs: function() {
-		this.$('.tab').remove();
-	},
+	clearTabs() {
+		this.$el.find('.tab').remove();
+	}
 
-	escapeName: function(name) {
-		return this.escapeNameTemplate({ name: name });
-	},
+	escapeName(name) {
+		return TabView.escapeNameTemplate({ name: name });
+	}
+}
 
-	tabTemplate: _.template(`
-		<div
-			class="tab <%- name ? 'tab-name-'+name : '' %>"
-			data-name="<%- name %>"></div>
-	`),
-	escapeNameTemplate: _.template('<%- name %>'),
-});
+TabView.tabTemplate = _.template(`
+	<div
+		class="tab <%- name ? 'tab-name-'+name : '' %>"
+		data-name="<%- name %>"></div>
+`);
+TabView.escapeNameTemplate = _.template('<%- name %>');
