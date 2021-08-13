8467a712aeaa4ffa921f4c0fcb702e46f082a87a
polyfloyd <floyd@polyfloyd.net>
2021-08-13 15:01:04 +0200

    web/track-art: Lazily load images to speed up loading times

--- src/handler/webui/static/components/track-art.js
+++ src/handler/webui/static/components/track-art.js
@@ -13,7 +13,20 @@ Vue.component('track-art', {
 			:style="{backgroundImage: blobUrl && 'url(\\''+blobUrl+'\\')'}"></div>
 	`,
 	mounted: function() {
-		this.load();
+		let elem = this.$el;
+		while (elem.parentElement) {
+			let w = elem.addEventListener('scroll', this.load)
+			elem = elem.parentElement;
+		}
+
+		setTimeout(this.load, 100);
+	},
+	beforeDestroy: function() {
+		let elem = this.$el;
+		while (elem.parentElement) {
+			let w = elem.removeEventListener('scroll', this.load)
+			elem = elem.parentElement;
+		}
 	},
 	watch: {
 		track: function() {
@@ -22,8 +35,29 @@ Vue.component('track-art', {
 		},
 	},
 	methods: {
+		visible: function() {
+			// An element is visible if a part of its bounding box is inside
+			// the bounding box of all its parents.
+			let elem = this.$el;
+			let rect = elem.getBoundingClientRect();
+			while (elem.parentElement) {
+				let parentRect = elem.parentElement.getBoundingClientRect();
+				elem = elem.parentElement;
+				rect = {
+					top: Math.max(rect.top, parentRect.top),
+					left: Math.max(rect.left, parentRect.left),
+					right: Math.min(rect.right, parentRect.right),
+					bottom: Math.min(rect.bottom, parentRect.bottom),
+				};
+			}
+			return (rect.bottom - rect.top) > 0 && (rect.right - rect.left) > 0;
+		},
 		load: async function() {
-			if (!this.track) return;
+			// Do not attempt to load if there is no track or an image has
+			// already been loaded.
+			if (!this.track || this.blobUrl) return;
+			// Do not load if the element is not visible to save resources.
+			if (!this.visible()) return;
 
 			let url = `${this.urlroot}data/player/${this.selectedPlayer}/tracks/art?track=${encodeURIComponent(this.track.uri)}`;
 
