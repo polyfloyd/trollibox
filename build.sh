e9e29c5ab56f69db86e6e559d3aa9c16dbbeb511
polyfloyd <floyd@polyfloyd.net>
2021-08-13 22:55:32 +0200

    Fix release build

--- build.sh
+++ build.sh
@@ -13,14 +13,17 @@ GO_MINIFY="github.com/tdewolff/minify/v2/cmd/minify"
 mkdir -p "$BIN"
 mkdir -p "$LIB"
 
+rm "$ASSETS/static/js/app.js" || true
+rm "$ASSETS/static/css/app.css" || true
+
 echo "*** Building Project ***"
 if [ ${RELEASE:-} ]; then
-    rm "$ASSETS/static/js/app.js" || true
+    mkdir -p "$ASSETS/static/js"
     cat `find "$ASSETS" -name "*.js" | sort` \
         | go run $GO_MINIFY --type=js \
         > "$ASSETS/static/js/app.js"
 
-    rm "$ASSETS/static/css/app.css" || true
+    mkdir -p "$ASSETS/static/css"
     cat `find "$ASSETS" -name "*.css" | sort` \
         | go run $GO_MINIFY --type=css \
         > "$ASSETS/static/css/app.css"
