5a6d7246234bac1a45950e7f20d2b80ed21df71c
polyfloyd <floyd@polyfloyd.net>
2022-08-31 20:18:51 +0200

    test: Add test for meta keeper ordering preservation

--- src/player/keeper_test.go
+++ src/player/keeper_test.go
@@ -2,6 +2,7 @@ package player
 
 import (
 	"context"
+	"reflect"
 	"testing"
 
 	"trollibox/src/library"
@@ -36,6 +37,68 @@ func TestMetaKeeperPlaylistImplementation(t *testing.T) {
 	}
 	metapl := &PlaylistMetaKeeper{Playlist: &DummyPlaylist{}}
 	TestPlaylistImplementation[MetaTrack](t, metapl, tracks)
+}
+
+func TestMetaKeeperListTracks(t *testing.T) {
+	ctx := context.Background()
+	tracks := []MetaTrack{
+		{
+			Track: library.Track{
+				URI:    "track1",
+				Artist: "Artist 1",
+				Title:  "Title 1",
+			},
+			QueuedBy: "foo",
+		},
+		{
+			Track: library.Track{
+				URI:    "track2",
+				Artist: "Artist 2",
+				Title:  "Title 2",
+			},
+			QueuedBy: "bar",
+		},
+		{
+			Track: library.Track{
+				URI:    "track3",
+				Artist: "Artist 3",
+				Title:  "Title 3",
+			},
+			QueuedBy: "baz",
+		},
+	}
+	metapl := &PlaylistMetaKeeper{Playlist: &DummyPlaylist{}}
+	if err := metapl.Insert(ctx, 0, tracks...); err != nil {
+		t.Fatal(err)
+	}
+
+	queuedBys := func() []string {
+		tracks, err := metapl.Tracks(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		qq := make([]string, len(tracks))
+		for i, t := range tracks {
+			qq[i] = t.QueuedBy
+		}
+		return qq
+	}
+
+	if qq := queuedBys(); !reflect.DeepEqual(qq, []string{"foo", "bar", "baz"}) {
+		t.Fatalf("Unexpected QueuedBy's: %q", qq)
+	}
+	if err := metapl.Insert(ctx, -1, MetaTrack{Track: tracks[0].Track, QueuedBy: "foo"}); err != nil {
+		t.Fatal(err)
+	}
+	if qq := queuedBys(); !reflect.DeepEqual(qq, []string{"foo", "bar", "baz", "foo"}) {
+		t.Fatalf("Unexpected QueuedBy's: %q", qq)
+	}
+	if err := metapl.Insert(ctx, 0, MetaTrack{Track: tracks[0].Track, QueuedBy: "qux"}); err != nil {
+		t.Fatal(err)
+	}
+	if qq := queuedBys(); !reflect.DeepEqual(qq, []string{"qux", "foo", "bar", "baz", "foo"}) {
+		t.Fatalf("Unexpected QueuedBy's: %q", qq)
+	}
 }
 
 func TestMetaKeeperInsert(t *testing.T) {
